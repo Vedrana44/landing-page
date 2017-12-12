276c8f914f2761b8a22980038a43e2ca75132bb5
Vedrana44 <vedrana.vekic4@gmail.com>
2017-12-12 10:28:15 +0100

    Landing page

---
+++ app/js/main.js
@@ -0,0 +1,17 @@
+// TABS
+var question = document.getElementsByClassName("question");
+var i;
+
+for (i = 0; i < question.length; i++) {
+    question[i].onclick = function() {
+    this.classList.toggle("active");
+    var answer = this.nextElementSibling;
+    if (answer.style.maxHeight){
+        answer.style.maxHeight = null;
+    } else {
+        answer.style.maxHeight = answer.scrollHeight + "px";
+    } 
+  }
+}
+
+
